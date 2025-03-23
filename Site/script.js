4c8731e5fceb93b6a9e4d61c54d44df6e8489c63
Henry Kaique <henry.reis4@etec.sp.gov.br>
2025-03-22 21:04:56 -0300

    Novos personagens adicionados

--- Site/script.js
+++ Site/script.js
@@ -18,7 +18,29 @@ document.addEventListener("DOMContentLoaded", function() {
         "Marina": {
             description: "Marina é uma ocultista que se concentra no uso criativo de poderes de outro mundo.Embora não seja tão forte em habilidades ofensivas, suas capacidades não são menos impressionantes quando usadas corretamente. Ela é uma das quatorze participantes do Festival de Termina.",
             image: "Marina.jpg"
+        },
+        "Tanaka": {
+            description: "Tanaka é um empresário de Edo que viajou para Europa após a Segunda Grande Guerra para expandir os negócios de sua família. Ele possui uma posição importante na empresa de seu pai, mas suas habilidades comerciais não são particularmente úteis para a sobrevivência. Tanaka é um dos quatorze participantes forçados a participar do Festival de Termina",
+            image: "Tanaka.jpg"
+        },
+        "Abella": {
+            description: "Abella é uma mecânica do Reino de Oldegård. Ela é uma das participantes do Festival de Termina. ",
+            image: "Abella.jpg"
+        },
+        "Osaa": {
+            description: "O'saa é um Mago Amarelo de Abyssonia e aprendiz do poderoso mago Nas'hrah. Ele é um dos participantes do Festival de Termina",
+            image: "Osaa.jpg"
+        },
+        "Samarie": {
+            description: "Samarie é uma mulher misteriosa que está obcecada por Marina. Ela é uma das participantes do Festival de Termina.",
+            image: "Samarie.jpg"
+        },
+        "Olivia": {
+            description: "Olivia Haas é uma botânica em cadeira de rodas do Império de Bremen. Sua irmã, Reila Audrey Haas, desempenha um papel importante no jogo como a entidade mecânica conhecida como Lógica. Olivia é uma das participantes do Festival de Termina.",
+            image: "Olívia.jpg"
         }
+        
+
     };
 
     document.querySelectorAll("#characters a").forEach(link => {
